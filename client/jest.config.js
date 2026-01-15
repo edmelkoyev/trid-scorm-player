2f85982fc0c1269855f8dd63935f31583c3f6318
Alexei Delezhov <lexadelejov@gmail.com>
2026-01-15 16:07:09 +0300

    fix config

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transformIgnorePatterns: [
-    '/node_modules/'
-  ]
 };
